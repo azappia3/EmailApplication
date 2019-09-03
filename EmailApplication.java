4b6dbb135baef64f19e989abf7148e99eea9be11
Andrew Zappia <zappia.andrew@yahoo.com>
2019-09-03 15:18:48 -0500

    Add files via upload

---
+++ EmailApplication.java
@@ -0,0 +1,11 @@
+package emailapplication;
+
+public class EmailApplication {
+
+	public static void main(String[] args) {
+		Email em1 = new Email("John" , "Smith");
+		System.out.println(em1.showInfo());
+
+	}
+
+}
